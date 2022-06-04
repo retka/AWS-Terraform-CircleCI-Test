e8ce77f05999dc0882f5005588d05c6b5695b64e
Retka <christopherpickard6@gmail.com>
2022-06-04 17:31:18 -0400

    Created simple Terraform project that creates a single DynamoDB table. Different .tfvars files are used to manage different environments in the same account.

---
+++ database.tf
@@ -0,0 +1,10 @@
+resource "aws_dynamodb_table" "compatibility" {
+    name         = "${var.env}-compatibility"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key     = "PK"
+
+    attribute {
+        name = "PK"
+        type = "S"
+    }
+}
