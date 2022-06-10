4b7ce316f14536fe637cb5ba19045644b10106a3
Christopher Pickard <christopherpickard6@gmail.com>
2022-06-09 20:59:10 -0400

    tf-provider-generate.py now reads variables for provider.tf from .tfvars.json files provided as a string of the path to the file in the -var-file argument. This allows you to specify per-environment state bucket, state key, region, and profile all through your envs tfvars files.

--- tf-provider-generate.py
+++ tf-provider-generate.py
@@ -1,10 +1,21 @@
 from string import Template
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description="testdesc")
+parser.add_argument("-var-file", required=True, type=str, help="Path to the .tfvars.json file for the environment you want to deploy.")
+args = parser.parse_args()
+
+var_file = args.var_file
+
+with open(var_file, "r") as f:
+    tfvars = json.load(f)
 
 params = {
-    "bucket_name": "REDACTED",
-    "state_key"  : "test.tfstate",
-    "region"     : "us-east-1",
-    "aws_profile": "chrisdev_admin"
+    "bucket_name": tfvars["state_bucket"],
+    "state_key"  : f'{tfvars["env"]}.tfstate',
+    "region"     : tfvars["region"],
+    "aws_profile": tfvars["aws_profile"]
 }
 
 with open('provider.tf.tmpl', 'r') as tmplFile:
